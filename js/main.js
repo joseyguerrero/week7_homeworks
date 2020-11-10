67f1645ec0d211ac0008ae0aa52db21471367073
joseyguerrero <71401409+joseyguerrero@users.noreply.github.com>
2020-11-10 09:35:40 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+"Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+*/
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max","HAS","PuRple","dog"]
+
+function findWords(){
+    //Your code goes here
+}
+
+//Call method here with parameters
+
+
+
+
+
+//============Exercise #2 ============//
+/*Write a fucntion that takes in an array and removes every even index with a splice,
+and replaces it with the string "even index" */
+
+Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+
+function replaceEvens(arr){
+    for(let i = 0; i < replaceEvens.length; i++){
+        if(i % 2 == 0){
+            replaceEvens.splice(i,1,'Goku')
+        }
+        console.log(groupOfNames)
+}
+
+}
+
+//Expected output
+//Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+//Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
